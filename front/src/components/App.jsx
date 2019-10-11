ec1f618ec88d7b803bee1a85a95d55d8b9adbe36
Krzysztof <dydakkrzysztof@gmail.com>
2019-10-11 22:27:42 +0200

    implemented redux into App

--- front/src/components/App.jsx
+++ front/src/components/App.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 
 export default function App() {
-	console.log(darkTheme);
 	return (
-		<HashRouter>
-			<Switch></Switch>
-		</HashRouter>
+		<div>
+			<h1>Hello I'm working!</h1>
+		</div>
 	);
 }
--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
 import "./global.css";
 
 import App from "./components/App";
+import store from "./redux";
 
 const root = document.querySelector("#root");
 
-render(<App />, root);
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	root
+);
