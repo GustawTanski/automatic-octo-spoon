78d5345c6bc17b5efdade25dd88c4148c32ed1fb
deBroglie <tanski.gustaw@gmail.com>
2019-10-11 21:31:51 +0200

    oink!

---
+++ front/config-overrides.js
@@ -0,0 +1,15 @@
+const { override, fixBabelImports, addLessLoader } = require("customize-cra");
+const darkTheme = require("@ant-design/dark-theme");
+console.log(darkTheme);
+
+module.exports = override(
+	fixBabelImports("import", {
+		libraryName: "antd",
+		libraryDirectory: "es",
+		style: true
+	}),
+	addLessLoader({
+		javascriptEnabled: true,
+		modifyVars: darkTheme.default
+	})
+);
--- front/src/components/App.jsx
+++ front/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 
 export default function App() {
+	console.log(darkTheme);
 	return (
 		<HashRouter>
 			<Switch></Switch>
