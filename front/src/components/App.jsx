d86451a770bebe57a74c636e56d19242c20a41b4
Damian Borowy <tymooteuusz@gmail.com>
2019-10-12 00:35:59 +0200

    Added some basic styling
    It do be looking kinda sexy tho

--- front/src/components/App.jsx
+++ front/src/components/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import { Button } from "antd";
-import showFeedbackModal from "./FeedbackModal";
+import showFeedbackModal from "./FeedbackModal/";
 
 export default function App() {
 	return (
 		<HashRouter>
-            <Switch>
-                <Button onClick={showFeedbackModal}>Show a modal</Button>
-            </Switch>
+			<Switch>
+				<Button onClick={showFeedbackModal}>Show a modal</Button>
+			</Switch>
 		</HashRouter>
 	);
 }
