10683da1d2fa78b681cd3ce6e40cae607990b592
Krzysztof <dydakkrzysztof@gmail.com>
2019-10-12 06:06:25 +0200

    Refactor

--- front/src/redux/actions/tags.js
+++ front/src/redux/actions/tags.js
@@ -1,63 +1,6 @@
-import { POST_TAG, GET_ALL_TAGS } from "./actionGenerator";
+import { POST_TAG, GET_ALL_TAGS, generateAsyncAction } from "./actionGenerator";
 import requestApi from "../api/index";
 
-export function postTag(tagName) {
-	return dispatch => {
-		requestApi
-			.post(
-				"/tags",
-				{
-					tagName
-				}.then(res => {
-					dispatch(postTagSuccess());
-				})
-			)
-			.catch(err => {
-				dispatch(postTagFailure());
-			});
-	};
-}
-
-const postTagSuccess = () => {
-	return {
-		type: POST_TAG.SUCCESS
-	};
-};
-
-const postTagFailure = () => {
-	return {
-		type: POST_TAG.FAILURE
-	};
-};
-
-export function getAllTags() {
-	return dispatch => {
-		dispatch(getAllTagsStarted());
-
-		requestApi
-			.get("/tags")
-			.then(res => {
-				dispatch(getAllTagsSuccess(res));
-			})
-			.catch(err => {
-				dispatch(getAllTagsFailure(err.message));
-			});
-	};
-}
-
-const getAllTagsSuccess = () => {
-	return {
-		type: GET_ALL_TAGS.SUCCESS
-	};
-};
-
-const getAllTagsFailure = () => {
-	return {
-		type: GET_ALL_TAGS.FAILURE
-	};
-};
-const getAllTagsStarted = () => {
-	return {
-		type: GET_ALL_TAGS.STARTED
-	};
+export const postTag = ({ tagName }) => {
+	return generateAsyncAction(requestApi.post("/tags", { name: tagName }));
 };
