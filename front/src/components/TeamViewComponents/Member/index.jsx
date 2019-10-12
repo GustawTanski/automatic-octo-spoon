56864d603622a6750ffb9a72c94ab8ba4ae70dba
deBroglie <tanski.gustaw@gmail.com>
2019-10-12 07:39:19 +0200

    styled

--- front/src/components/TeamViewComponents/Member/index.jsx
+++ front/src/components/TeamViewComponents/Member/index.jsx
@@ -10,7 +10,10 @@ import "./index.css";
 export default function Member({ name, position, chef, id }) {
 	const color = useColor(name);
 	return (
-		<List.Item className="member__wrapper" actions={[<Actions color={color} id={id} name={name} />]}>
+		<List.Item
+			className="member__wrapper"
+			actions={[<Actions color={color} id={id} name={name} />]}
+		>
 			<List.Item.Meta
 				avatar={<Avatar name={name} color={color} />}
 				title={<Title name={name} position={position} chef={chef} color={color} />}
