7ab261b54fede584cdf3728614a97f3b119ffa0f
Damian Borowy <tymooteuusz@gmail.com>
2019-10-12 04:10:21 +0200

    Wrapped component in Form

--- front/src/components/FeedbackModal/index.jsx
+++ front/src/components/FeedbackModal/index.jsx
@@ -1,33 +1,30 @@
 import React from "react";
-import { Modal, Input, Select, Rate, Radio } from "antd";
+import { Modal, Input, Select, Rate, Radio, Form } from "antd";
 import TextArea from "antd/lib/input/TextArea";
-import { connect } from "react-redux";
-import { postFeedback } from "../../"
 import "./style.css";
 
 const { confirm } = Modal;
 const { Group, Button } = Radio;
+const { Item } = Form;
 
 export default function showFeedbackModal(username) {
 	confirm({
 		title: `Submit a feedback ${username ? "to " + username : ""}`,
-		content: <Feedback />,
-		icon: null,
+		content: <FormFeedback />,
 		okText: "Submit",
+		centered: true,
+		icon: null,
 		onOk
 	});
 }
 
 function onOk() {
 	// TODO:: Implement
-    setTimeout(() => {
-        
-    }, 2000);
 }
 
 class FeedbackModal extends React.Component {
 	state = { selectedRadio: "anonymous" };
-	tagsList = [1, 2, 3, "dupa", true].map(elem => <Select.Option key={elem}>{elem}</Select.Option>);
+	tagsList = [1, 2, 3, "dupa"].map(elem => <Select.Option key={elem}>{elem}</Select.Option>);
 
 	selectRadio = event => {
 		this.setState({
@@ -35,55 +32,78 @@ class FeedbackModal extends React.Component {
 		});
 	};
 
+	check = event => {
+		event.preventDefault();
+		this.props.form.validateFields((err, values) => {
+			if (!err) {
+				console.log("Received values of form: ", values);
+			}
+		});
+	};
+
 	render() {
+		const { getFieldDecorator } = this.props.form;
+
 		return (
-			<div className="modal">
-				<Input placeholder="Title" />
-				<Select
-					ref={this.selectRef}
-					mode="tags"
-					className="modal__select"
-					placeholder="Tags"
-					showArrow
-				>
-					{this.tagsList}
-				</Select>
+			<Form className="modal">
+				<Item>
+					{getFieldDecorator("title", {
+						rules: [
+							{
+                                required: true,
+                                max: 64,
+								message: "The title is required"
+							}
+						]
+					})(<Input placeholder="Title" />)}
+				</Item>
+				<Item>
+					{getFieldDecorator("select")(
+						<Select
+							ref={this.selectRef}
+							mode="tags"
+							className="modal__select"
+							placeholder="Tags"
+							showArrow
+						>
+							{this.tagsList}
+						</Select>
+					)}
+				</Item>
 				{this.state.selectedRadio !== "rating" && (
-					<TextArea placeholder="Content" autosize={{ minRows: 2, maxRows: 8 }} />
+					<Item>
+						{getFieldDecorator("content", {
+							rules: [
+								{
+                                    required: true,
+                                    max: 2048,
+									message: "Feedback's message is required"
+								}
+							]
+						})(<TextArea placeholder="Content" autosize={{ minRows: 2, maxRows: 8 }} />)}
+					</Item>
 				)}
-				<Group
-					defaultValue="anonymous"
-					buttonStyle="solid"
-					className="modal__radio-group"
-					onChange={e => this.selectRadio(e)}
-				>
-					<Button value="public">Public</Button>
-					<Button value="anonymous">Anonymous</Button>
-					<Button value="rating">Only rating</Button>
-				</Group>
-				<Rate className="modal__rating" allowHalf />
-			</div>
+				<Item>
+					{getFieldDecorator("privacy", {initialValue: "anonymous"})(
+                        <Group
+							// defaultValue="anonymous"
+							buttonStyle="solid"
+							className="modal__radio-group"
+							onChange={e => this.selectRadio(e)}
+						>
+							<Button value="public">Public</Button>
+							<Button value="anonymous">Anonymous</Button>
+							<Button value="rating">Only rating</Button>
+						</Group>
+					)}
+				</Item>
+				<Item>{getFieldDecorator("rate")(<Rate className="modal__rating" allowHalf />)}</Item>
+				<Item>
+					<Button onClick={e => this.check(e)}>Dupa</Button>
+				</Item>
+			</Form>
 		);
 	}
 }
 
-const mapDispatchToProps = dispatch => {
-	return {
-		onPost: (title, body) => {
-			dispatch(postFeedback(title, body));
-		}
-	};
-};
-
-const mapStateToProps = state => {
-	return {
-		isPending: state.feedbackPostReducer.isPending
-	};
-};
-
-const Feedback = connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(FeedbackModal);
-
-// const Feedback = FeedbackModal;
+const FormFeedback = Form.create({})(FeedbackModal);
