9ba328336bc8e2c3b1f72536c024775fe8c5738e
unknown <filipzynwala@gmail.com>
2019-10-12 02:20:10 +0200

    Add login route without JWT

--- back/src/app.js
+++ back/src/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const connectdb = require('./users/connectDB');
 const model = require('./users/userModel.js');
 const users = require('./users/routers/routers');
+const login = require('./users/routers/login');
 
 const app = express();
 
@@ -14,10 +15,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/users', users);
+app.use('/login', login);
 
-
-
-//model.createUser('Mr Bean', 'bean@gmail.com', 'mrbean', 'admin');
 
 const port = process.env.PORT || 3000;
 app.listen(port);
---
+++ back/src/users/routers/login.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const model = require('../userModel.js');
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    console.log('Hello from login POST');
+    console.log(req.body);
+
+    const user = await model.User.findOne({email: req.body.email});
+    if (!user) return res.status(404).send('Invalid email or password');
+
+    const valid = await bcrypt.compare(req.body.password, user.password);
+    if(!valid) return res.status(400).send('Invalid email or password');
+
+    res.send(true);
+
+});
+
+module.exports = router;
