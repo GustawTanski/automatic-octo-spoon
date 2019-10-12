4b43dcc23446aac79c143cf47cdfc4987efd8fba
unknown <filipzynwala@gmail.com>
2019-10-12 08:00:49 +0200

    Add team to login route

--- back/src/users/routers/login.js
+++ back/src/users/routers/login.js
@@ -5,22 +5,23 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userModel = require('../models/userModel');
+const teamModel = require('../models/teamModel');
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    console.log('Hello from login POST');
-    console.log(req.body);
-
+    let team = await teamModel.Team.findOne({name: req.body.team});
     const user = await userModel.User.findOne({email: req.body.email});
+
     if (!user) return res.status(404).send('Invalid email or password');
+    if(!team) return res.status(400).send('The team does not exist');
 
     const valid = await bcrypt.compare(req.body.password, user.password);
     if(!valid) return res.status(400).send('Invalid email or password');
 
     const token = jwt.sign({_id: user._id, isBoss: user.isBoss}, 'jwtPrivateKey');
 
-    res.header('x-auth-token', token).send();
+    res.header('x-auth-token', token).send(team.name);
 
 });
 
