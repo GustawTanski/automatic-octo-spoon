5c23c33ce735f928da6e2d70b0ab9dac2dfa8d24
Patryk Wydmuch <pvydmuch@gmail.com>
2019-10-12 03:34:50 +0200

    Add search icon

--- front/src/components/ViewFeedback/FeedbackSider.jsx
+++ front/src/components/ViewFeedback/FeedbackSider.jsx
@@ -1,49 +1,80 @@
-import { Layout, Menu, Breadcrumb, Icon, Input, Select } from 'antd';
-import React from "react"
-import SiderSelect from "./SiderSelect"
-const { Header, Content, Footer, Sider } = Layout;
+import React from "react";
+import { connect } from "react-redux"
+import { Layout, Menu, Icon, Input, Select } from "antd";
+
+const { Sider } = Layout;
+
 const { SubMenu } = Menu;
-const { Option } = Select
+const { Option } = Select;
 const children = [];
-for (let i = 10; i < 36; i++) {
-  children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
-}
+
+
+
 export default class FeedbackSider extends React.Component {
     state = {
-        collapsed: false,
+        collapsed: false
     };
-
+    optionRef = React.createRef();
     onCollapse = collapsed => {
         console.log(collapsed);
         this.setState({ collapsed });
     };
 
-
-
+    // componentWillMount(){
+    //     for (let i = 10; i < 11; i++) {
+    //         children.push(<Option ref={this.optionRef} style={{border: "solid red 2px" ,display: "flex", justifyContent: "space-be"}}  key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
+    //     }
+    //     children.push(<Option key="38" >sdfsdfdksjadskfhadkfh</Option>);
+    // }
+    componentDidUpdate(){
+        this.optionRef.current.style.border = "yellow solid 1px"
+    }   
     render() {
+       const selectStyle = !this.state.collapsed ? { width: "80%" }: { width: "80%", display:'none' };
         return (
-            <Layout style={{ minHeight: '100vh' }}>
-                <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+            <Layout style={{ minHeight: "100vh" }}>
+                <Sider  collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
                     <div className="logo" />
-
-                    {/* <SiderSelect/> */}
-
-
-                    <Select
-                        mode="multiple"
-                        style={{ width: '100%' }}
-                        placeholder="Please select category"
-                        defaultValue={['a10', 'c12']}
-                        suffixIcon={<Icon type="search" />}
-                    // onChange={handleChange}
-                    >{children}
-                    </Select>
-                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                        <Menu.Item key="1">
+                    {/* <Select
+                               style={selectStyle}
+                                mode="multiple"
+                                // style={{ width: "80%" }}
+                                placeholder="Select category"
+                                // defaultValue={["a10", "c12"]}
+                                // suffixIcon={<Icon type="search" />}
+                            // onChange={handleChange}
+                            >
+                                {children}
+                            </Select> */}
+                    <Menu theme="dark" defaultSelectedKeys={["1"]} mode="vertical">
+                        <Menu.Item style={{height:"auto"}} key="1" title="Search" onClick={()=>{
+                            if(this.state.collapsed){
+                            this.setState({collapsed:false})
+                            }
+                        }} >
+                            <Icon type="search" />
+                   
+                    
+                            <Select
+                               style={selectStyle}
+                                mode="multiple"
+                                // style={{ width: "80%" }}
+                                placeholder="Select category"
+                                // defaultValue={["a10", "c12"]}
+                                // suffixIcon={<Icon type="search" />}
+                            // onChange={handleChange}
+                            >
+                                {/* {children} */}
+                                <Option ref={this.optionRef} style={{border: "solid red 2px" ,display: "flex"}} key="1">1</Option>
+                            </Select>
+                            
+                        </Menu.Item>
+                       
+                        <Menu.Item key="2">
                             <Icon type="pie-chart" />
                             <span>Option 1</span>
                         </Menu.Item>
-                        <Menu.Item key="2">
+                        <Menu.Item key="3">
                             <Icon type="desktop" />
                             <span>Option 2</span>
                         </Menu.Item>
@@ -56,9 +87,9 @@ export default class FeedbackSider extends React.Component {
                                 </span>
                             }
                         >
-                            <Menu.Item key="3">Tom</Menu.Item>
-                            <Menu.Item key="4">Bill</Menu.Item>
-                            <Menu.Item key="5">Alex</Menu.Item>
+                            <Menu.Item key="4">Tom</Menu.Item>
+                            <Menu.Item key="5">Bill</Menu.Item>
+                            <Menu.Item key="6">Alex</Menu.Item>
                         </SubMenu>
                         <SubMenu
                             key="sub2"
@@ -78,17 +109,6 @@ export default class FeedbackSider extends React.Component {
                         </Menu.Item>
                     </Menu>
                 </Sider>
-                <Layout>
-                    <Header style={{ background: '#fff', padding: 0 }} />
-                    <Content style={{ margin: '0 16px' }}>
-                        <Breadcrumb style={{ margin: '16px 0' }}>
-                            <Breadcrumb.Item>User</Breadcrumb.Item>
-                            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                        </Breadcrumb>
-                        <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div>
-                    </Content>
-                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-                </Layout>
             </Layout>
         );
     }
--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
 import "./global.css";
-import Sider from "./components/ViewFeedback/FeedbackSider"
+import Sider from "./components/ViewFeedback/FeedbackSider";
 
 // import App from "./components/App";
 
 const root = document.querySelector("#root");
 
-render(<Sider/>, root);
+render(<Sider />, root);
